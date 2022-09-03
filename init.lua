90002864c8b0af013c91c917980e0c796ca6e3de
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-09-03 19:26:41 +0200

    Short the init file
    Made a file with the name of settings.lua for all the settings I use to
    make the init only for requiring (importing) that files

--- init.lua
+++ init.lua
@@ -1,17 +1,17 @@
--- Options
-vim.o.number = true
-vim.o.tabstop = 4
-vim.o.smarttab = true
-vim.o.autoindent = true
-vim.o.shiftwidth = 2
-vim.o.relativenumber = true
-vim.opt.mouse = "a"
+-- Settings (numbers, tab, etc...)
+require('settings')
+
 vim.cmd([[set splitbelow splitright]])
 vim.cmd([[set encoding=utf8]])
 -- vim.cmd([[set guifont=Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete 11]])
 
 -- plugins
-require('plugin')
-require('colorscheme')
+require('plugin') 
 require('nvim-autopairs').setup {}
+
+-- Colorscheme
+require('colorscheme')
+
+-- shortcuts
 require('shortcuts')
+
---
+++ lua/settings.lua
@@ -0,0 +1,8 @@
+-- Options
+vim.o.number = true
+vim.o.tabstop = 4
+vim.o.smarttab = true
+vim.o.autoindent = true
+vim.o.shiftwidth = 2
+vim.o.relativenumber = true
+vim.opt.mouse = "a"
