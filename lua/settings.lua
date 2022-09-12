747b6d87f476f22729af83c6939b6ab3d3d8624a
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-09-12 19:33:28 +0200

    Refactor the settings
    - Re-organized the settings
    - Add more settings

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,8 +1,26 @@
--- Options
-vim.o.number = true
+-- -==*==- Options -==*==- --
+
+-- Tabs
 vim.o.tabstop = 4
+vim.o.softtabstop = 4
 vim.o.smarttab = true
-vim.o.autoindent = true
+vim.o.smartindent = true
+vim.o.expandtab = true
 vim.o.shiftwidth = 2
+vim.o.autoindent = true
+
+-- numbers
+vim.o.number = true
 vim.o.relativenumber = true
+
+-- UX 
+vim.o.spell = true
 vim.opt.mouse = "a"
+vim.opt.clipboard = 'unnamedplus'
+
+-- styling
+vim.opt.hlsearch = false
+vim.opt.hidden = true
+vim.opt.wrap = false
+vim.opt.scrolloff = 7
+vim.opt.wildmode = {'longest', 'list', 'full'}
