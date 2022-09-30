e3303fa6aa3c28855ccec50f60dd580e7ebdb61e
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-09-30 07:55:37 +0200

    Remove unneeded comments

--- init.lua
+++ init.lua
@@ -3,10 +3,8 @@ require('settings')
 
 vim.cmd([[set splitbelow splitright]])
 vim.cmd([[set encoding=utf8]])
--- vim.cmd([[set guifont=Droid\ Sans\ Mono\ for\ Powerline\ Nerd\ Font\ Complete 11]])
 
 -- plugins
--- require('plugin') 
 require('plugins')
 
 -- Colorscheme
