5a5984c9a9971bea0093b8d2ede4989efb1cf568
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-10-05 23:50:36 +0200

    Add tabline plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -147,5 +147,30 @@ return require('packer').startup(function()
     run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' 
   }
 
+  use {
+    'kdheepak/tabline.nvim',
+    config = function()
+      require'tabline'.setup {
+          enable = true,
+          options = {
+            show_tabs_always = false, -- this shows tabs only when there are more than one tab or if the first tab is named
+            show_devicons = true, -- this shows devicons in buffer section
+            show_filename_only = true, -- shows base filename only instead of relative path in filename
+            modified_italic = false, -- set to true by default; this determines whether the filename turns italic if modified
+            show_tabs_only = true, -- this shows only tabs instead of tabs + buffers
+            section_separators = {'', ''},
+            component_separators = {'', ''},
+          }
+      }
+      -- vim.cmd[[
+      --   set guioptions-=e " Use showtabline in gui vim
+      --   set sessionoptions+=tabpages,globals " store tabpages and globals in session
+      -- ]]
+    end,
+    requires = { 
+      {'kyazdani42/nvim-web-devicons', opt = true} 
+    }
+  }
+
 end)
 
