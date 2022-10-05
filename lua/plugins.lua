a5d8acbf38ef715c7bb5973e578ce23726e36bd8
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-10-05 23:48:00 +0200

    Enhance the lualine config code

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -48,7 +48,9 @@ return require('packer').startup(function()
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = function() require('lualine').setup {} end
+    config = function()
+      require('lualine').setup {} 
+    end
   }
 
   -- Coc intelligence 
