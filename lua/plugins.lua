7a745b68da4d99320dd85477e10611cfbb256b04
AbdelrahmanDwedar <dedou2005@gmai.com>
2022-10-24 16:38:41 +0200

    Enhance the plugins file
    Removed all useless spaces & tables

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -60,105 +60,35 @@ return require('packer').startup(function()
   use {
     'nvim-lualine/lualine.nvim',
     requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-    config = function()
-      require('lualine').setup {} 
-    end
+    config = function() require('lualine').setup {} end
   }
 
   -- lsp support (languages support protocol)
-  use {
-    'neovim/nvim-lspconfig'
-  }
+  use 'neovim/nvim-lspconfig'
   -- auto conpletion for `"({[ etc..
   use {
   	"windwp/nvim-autopairs",
-    config = function() 
-      require("nvim-autopairs").setup {} 
-    end
+    config = function() require("nvim-autopairs").setup {} end
   }
   -- discord presence for neovim
-  use {
-    'andweeb/presence.nvim',
-    config = function() 
-      require('presence'):setup({
-        auto_update = true,
-        neovim_image_text = "NeoVim",
-		main_image = "neovim",
-		client_id = "793271441293967371", -- Use your own Discord application client id (not recommended)
-		log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
-		debounce_timeout = 10,
-		enable_line_number = false,
-		blacklist = {},
-		buttons = false,
-
-		-- Rich Presence text options
-		editing_text = "Editing %s",
-		file_explorer_text = "Browsing %s",
-		git_commit_text = "Committing changes",
-		plugin_manager_text = "Managing plugins", -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
-		reading_text = "Reading %s",
-		workspace_text = "Project: %s",
-      })
-    end
-  }
+  use 'andweeb/presence.nvim'
   
   -- Telescope for navigation
   use {
     'nvim-telescope/telescope.nvim',
-    requires = {
-      'nvim-lua/plenary.nvim'
-    }
+	requires = 'nvim-lua/plenary.nvim'
   }
 
   -- NeoVim Tree written in lua
   use {
     'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icons
-    },
-    config = function()
-      require("nvim-tree").setup({
-        sort_by = "case_sensitive",
-        hijack_cursor = true,
-        view = {
-          adaptive_size = true,
-          centralize_selection = false,
-          mappings = {
-            list = {
-              { key = "u", action = "dir_up" },
-              { key = "t", action = "tabnew" },
-              { key = "s", action = "split" },
-              { key = "v", action = "vsplit" },
-              { key = "C", action = "cd" }
-            },
-          },
-        },
-        renderer = {
-          group_empty = true,
-        },
-        filters = {
-          dotfiles = false,
-          custom = {
-            '.git'
-          },
-        },
-      })
-    end
-  }
-
-  -- Markdown preview
-  use {
-    'iamcco/markdown-preview.nvim', 
-    run = 'cd app && yarn install', 
-    cmd = 'MarkdownPreview'
+	requires = 'kyazdani42/nvim-web-devicons',
   }
 
   -- For Commenting gcc & gc
   use {
     'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
+    config = function() require('Comment').setup() end
   }
 
   use({
