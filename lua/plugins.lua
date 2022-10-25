0e25b3204bcc08e9e4a2eee392256a8393a9b891
AbdelrahmanDwedar <dedou2005@gmai.com>
2022-10-25 21:43:38 +0200

    Add a running for  for treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -43,7 +43,10 @@ return require('packer').startup(function()
 	  }
   }
 
-  use "nvim-treesitter/nvim-treesitter"
+  use {
+	  "nvim-treesitter/nvim-treesitter",
+	  run = "TSUpdate"
+  }
 
   use "lewis6991/gitsigns.nvim"
 
