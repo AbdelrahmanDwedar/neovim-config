f32179a889773abbd0b7bd090092163eef706bcb
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-10-30 02:01:18 +0200

    Reorder plugins

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,96 +9,110 @@ vim.cmd([[
 
 return require('packer').startup(function()
 
-  use 'wbthomason/packer.nvim'
+	use 'wbthomason/packer.nvim'
 
-  -- === #### Native vim plugins ### === ---
+	-- === #### NeoVim plugins #### === --
 
-  -- wakatime watcher
-  use 'wakatime/vim-wakatime'
-
-  -- awesome vim colorschemes
-  use 'rafi/awesome-vim-colorschemes'
-
-  use 'mg979/vim-visual-multi'
-
-  -- === #### NeoVim plugins #### === --
-
-  -- lsp
-  use {
-	"neovim/nvim-lspconfig",
-	requires = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"jose-elias-alvarez/null-ls.nvim"
+	-- language server protocol 
+	use {
+		"neovim/nvim-lspconfig",
+		requires = {
+			"williamboman/mason.nvim",
+			"williamboman/mason-lspconfig.nvim",
+			"jose-elias-alvarez/null-ls.nvim"
+		}
 	}
-  }
 
-  use {
-	  "hrsh7th/nvim-cmp",
-	  requires = {
-		  "hrsh7th/cmp-buffer",
-		  "hrsh7th/cmp-path",
-		  "hrsh7th/cmp-nvim-lsp",
-		  "hrsh7th/cmp-nvim-lua"
-	  }
-  }
+	-- neovim completion
+	use {
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-nvim-lua"
+		}
+	}
 
-  use {
-	"nvim-treesitter/nvim-treesitter",
-	run = "TSUpdate"
-  }
+	-- neovim treesitter
+	use {
+		"nvim-treesitter/nvim-treesitter",
+		run = "TSUpdate"
+	}
 
-  use "lewis6991/gitsigns.nvim"
+	-- git signs integration 
+	use "lewis6991/gitsigns.nvim"
 
-  -- lua status bar
-  use { 
-	'nvim-lualine/lualine.nvim',
-	requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-	config = function() require('lualine').setup {} end
-  }
+	-- lua status bar
+	use { 
+		'nvim-lualine/lualine.nvim',
+		requires = { 'kyazdani42/nvim-web-devicons', opt = true },
+		config = function() require('lualine').setup {} end
+	}
 
-  use {
-	"windwp/nvim-autopairs",
-	config = function() require("nvim-autopairs").setup {} end
-  }
-  -- discord presence for neovim
-  use 'andweeb/presence.nvim'
+	-- auto completion for {([" etc...
+	use {
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup {} end
+	}
 
-  -- Telescope for navigation
-  use {
-	'nvim-telescope/telescope.nvim',
-	requires = 'nvim-lua/plenary.nvim'
-  }
+	-- discord presence for neovim
+	use 'andweeb/presence.nvim'
 
-  -- NeoVim Tree written in lua
-  use {
-	'kyazdani42/nvim-tree.lua',
-	requires = 'kyazdani42/nvim-web-devicons',
-  }
+	-- Telescope for navigation
+	use {
+		'nvim-telescope/telescope.nvim',
+		requires = 'nvim-lua/plenary.nvim'
+	}
 
-  -- For Commenting gcc & gc
-  use {
-	'numToStr/Comment.nvim',
-	config = function() require('Comment').setup() end
-  }
+	-- NeoVim Tree written in lua
+	use {
+		'kyazdani42/nvim-tree.lua',
+		requires = 'kyazdani42/nvim-web-devicons',
+	}
 
-  use({
-	  "kylechui/nvim-surround",
-	  tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-	  config = function() require("nvim-surround").setup() end
-  })
+	  -- For Commenting gcc & gc
+	use {
+		'numToStr/Comment.nvim',
+		config = function() require('Comment').setup() end
+	}
 
-  use {
-    'norcalli/nvim-colorizer.lua',
-    config = function() 
-		require 'colorizer'.setup({
-		  'css';
-		  'javascript';
-		  'html';
-		})
-	end}
+	-- shortcut to surround code with {([])} or tags etc...
+	use {
+		"kylechui/nvim-surround",
+		tag = "*", -- Use for stability; omit to use `main` branch for the latest features
+		config = function() require("nvim-surround").setup() end
+	}
 
-  use 'folke/tokyonight.nvim'
+	-- colorizing for hex and other color formats
+	use {
+		'norcalli/nvim-colorizer.lua',
+		config = function() 
+			require 'colorizer'.setup()
+		end
+	}
+
+	-- bufferline
+	use {
+		'akinsho/bufferline.nvim', 
+		tag = "v3.*", 
+		requires = 'kyazdani42/nvim-web-devicons'
+	}
+
+	-- best neovim colorschemes in one place
+	use '~/Programming/projects/cli/awesome-nvim-colorschemes'
+
+	-- === #### Native vim plugins ### === ---
+
+	-- wakatime watcher
+	use 'wakatime/vim-wakatime'
+
+	-- awesome vim colorschemes
+	use 'rafi/awesome-vim-colorschemes'
+
+	-- multi cursors visualy
+	use 'mg979/vim-visual-multi'
 
 end)
+
 
