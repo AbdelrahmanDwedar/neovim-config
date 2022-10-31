ca41be0db59ccd3c072272dee5877e2aa5ee8792
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-10-31 09:10:41 +0200

    Add LuaSnip

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -3,6 +3,13 @@ if not cmp_status_ok then
   return
 end
 
+local snip_status_ok, luasnip = pcall(require, "luasnip")
+if not snip_status_ok then
+  return
+end
+
+require("luasnip/loaders/from_vscode").lazy_load()
+
 local check_backspace = function()
   local col = vim.fn.col "." - 1
   return col == 0 or vim.fn.getline("."):sub(col, col):match "%s"
@@ -39,6 +46,11 @@ local kind_icons = {
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup {
+	snippet = {
+		expand = function(args)
+		  luasnip.lsp_expand(args.body) -- For `luasnip` users.
+		end,
+	  },
   mapping = {
     ["<C-k>"] = cmp.mapping.select_prev_item(),
 	["<C-j>"] = cmp.mapping.select_next_item(),
@@ -56,6 +68,10 @@ cmp.setup {
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
+      elseif luasnip.expandable() then
+        luasnip.expand()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       elseif check_backspace() then
         fallback()
       else
@@ -68,6 +84,8 @@ cmp.setup {
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
       else
         fallback()
       end
@@ -84,6 +102,7 @@ cmp.setup {
       -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
       vim_item.menu = ({
         nvim_lsp = "[LSP]",
+        luasnip = "[Snippet]",
         buffer = "[Buffer]",
         path = "[Path]",
       })[entry.source.name]
@@ -92,6 +111,7 @@ cmp.setup {
   },
   sources = {
     { name = "nvim_lsp" },
+    { name = "luasnip" },
     { name = "buffer" },
     { name = "path" },
   },
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -30,8 +30,14 @@ return require('packer').startup(function()
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
 			"hrsh7th/cmp-nvim-lsp",
-			"hrsh7th/cmp-nvim-lua"
+			"hrsh7th/cmp-nvim-lua",
+			"saadparwaiz1/cmp_luasnip"
 		}
+	}
+
+	use	{
+		"L3MON4D3/LuaSnip", 
+		tag = "vCurrentMajor.*"
 	}
 
 	-- neovim treesitter
