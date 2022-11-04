06a6375027540f0e821579834645658ecfdb6fd1
AbdelrahmanDwedar <dedou2005@gmail.com>
2022-11-04 13:35:18 +0200

    Chore(theme): Use the new themes configurations

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,11 +1,3 @@
-require('tokyonight').setup({
-	style = "night",
-	transparent = true,
-	terminal_colors = true,
-	styles = {
-		sidebars = "transparent", -- style for sidebars, see below
-		floats = "transparent",
-	}
-})
+require('config.themes')
 
 vim.cmd('colorscheme tokyonight')
